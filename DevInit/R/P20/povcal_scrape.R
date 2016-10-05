b40c66b7dfa42a2bba3c3d0d1bb657914b0307e2
Alex Miller <alex.k.miller@gmail.com>
2016-10-05 16:04:30 -0400

    Finished combining, need to fix agg by removing duplicates

--- DevInit/R/P20/povcal_scrape.R
+++ DevInit/R/P20/povcal_scrape.R
@@ -1,5 +1,8 @@
 library(Hmisc)
 library(data.table)
+library(zoo)
+library(plyr)
+library(WDI)
 
 wd <- "D:/Documents/Data/PovCal_Increment"
 setwd(wd)
@@ -73,3 +76,37 @@ ind <- subset(ind,!grepl("--",country,fixed=TRUE))
 
 ctryDiff <- setdiff(unique(ind$country),unique(agg$country))
 ind <- subset(ind,country %in% ctryDiff)
+#Only 1 year leads to interpolation trouble...
+ind <- subset(ind,country!="Japan")
+
+ind$svyYear <- ind$year
+all.perms <- expand.grid(unique(ind$country),unique(ind$year),unique(ind$pl))
+names(all.perms) <- c("country","year","pl")
+ind <- merge(ind,all.perms,by=c("country","year","pl"),all=TRUE)
+
+ind.interp <- ind[,.(
+  year=year
+  ,hc=na.approx(hc,rule=2)
+  ,svyYear=svyYear
+  ,original.hc=hc
+  ),by=.(country,pl)]
+
+indicator <- "SP.POP.TOTL"
+
+pop <- WDI(country = "all", 
+           indicator = indicator, 
+           start = 1981, 
+           end = 2013
+)
+
+ind.interp <- merge(ind.interp,pop,by=c("country","year"))
+ind.interp$pop <- ind.interp$SP.POP.TOTL/1000000
+remove <- c("original.hc","iso2c","SP.POP.TOTL")
+ind.interp[,(remove):=NULL]
+ind.interp$type <- "i"
+
+remove <- c("mean","pg","pg2","watts","detail")
+agg[,(remove):=NULL]
+
+pcn <- rbind(agg,ind.interp)
+save(pcn,file="pcn.RData")
