ab101d6d06c85504af9a25ebba1cf7f3537dcd98
Alex Miller <alex.k.miller@gmail.com>
2017-05-04 14:31:54 -0400

    Every country every year

--- DevInit/R/P20/atkinson_p20.R
+++ DevInit/R/P20/atkinson_p20.R
@@ -1,17 +1,14 @@
 library(data.table)
 source("C:/git/alexm-util/DevInit/R/P20/atkinson.R")
 
-# df <- data.frame(group=c("a","a","a","b","b","b"),L=c(0.3,0.1,1,0.2,0.4,1))
-# dt <- data.table(df)
-# setkey(dt,group,L)
-# dt[,diff:=c(L[1],diff(L)),by=group]
-
-dat <- read.csv("D:/Documents/Data/PovCal_Increment/years/pcn.2013.csv")
-keep <- c("country","pl","hc")
-dat <- dat[keep]
+# dat <- read.csv("D:/Documents/Data/PovCal_Increment/years/pcn.2013.csv")
+load("D:/Documents/Data/PovCal_Increment/pcn.RData")
+keep <- c("country","year","pl","hc")
+dat <- data.frame(pcn)[keep]
+dat <- dat[complete.cases(dat),]
 
 incomeGivenPercent <- function(perc,plV,hcV){
-  minDiff <- 100
+  minDiff <- 1000
   minIndex <- 0
   for(i in 1:length(hcV)){
     hc <- hcV[i]
@@ -31,11 +28,16 @@ dataIndex <- 1
 for(ctry in countries){
   message(ctry)
   ss <- subset(dat,country==ctry)
-  incomes <- sapply(seq(1,100),incomeGivenPercent,plV=ss$pl,hcV=ss$hc)
-  ai <- atkinson(incomes)
-  aiDF <- data.frame(country=ctry,atkinson=ai,year=2013)
-  dataList[[dataIndex]] <- aiDF
-  dataIndex <- dataIndex + 1
+  years <- unique(ss$year)
+  for(yr in years){
+    message(yr)
+    subss <- subset(ss,year==yr)
+    incomes <- sapply(seq(1,100),incomeGivenPercent,plV=subss$pl,hcV=subss$hc)
+    ai <- atkinson(incomes)
+    aiDF <- data.frame(country=ctry,atkinson=ai,year=yr)
+    dataList[[dataIndex]] <- aiDF
+    dataIndex <- dataIndex + 1
+  }
 }
 data <- rbindlist(dataList)
-write.csv(data,"atkinson.csv",row.names=FALSE)
+write.csv(data,"atkinson_over_time.csv",row.names=FALSE)
