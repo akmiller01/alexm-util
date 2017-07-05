072d00b40569172d07ca702ab52f4319ab5d1b5a
Alex Miller <alex.k.miller@gmail.com>
2017-07-05 16:03:09 -0400

    Download DHS from list and readdct

---
+++ DevInit/R/P20/readdct.R
@@ -0,0 +1,26 @@
+dct_file <- "C:/Users/Alex/Downloads/AFBR70FL/AFBR70FL.DCT"
+
+library(readr)
+
+readdct = function(dct_file){
+  first_line <- readLines(dct_file)[1]
+  file_name <- gsub('.*\\"(.*)\\".*', "\\1", first_line)
+  base_name <- basename(file_name)
+  file_path <- dirname(dct_file)
+  full_path_file <- paste(file_path,base_name,sep="/")
+  tab <- read.table(dct_file,header=FALSE,skip=2,sep="",fill=TRUE
+                    ,col.names=c("data.type","var.name","start.string1","start.string2")
+                    ,as.is=TRUE
+                    )
+  tab$start.string = paste(tab$start.string1,tab$start.string2)
+  tab <- subset(tab,start.string!=" ")
+  tab$start <- as.integer(gsub(".*\\:(.*)\\-.*", "\\1", tab$start.string))
+  tab$end <- as.integer(gsub(".*\\-(.*)", "\\1", tab$start.string))
+  tab$width <- (tab$end-tab$start)+1
+  dat <- read_fwf(full_path_file
+                 ,fwf_widths(tab$width,col_names=tab$var.name)
+  )
+  return(data.frame(dat))
+}
+
+dat <- readdct(dct_file)
