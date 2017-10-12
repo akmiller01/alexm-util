2babce501d5c7fb19a9bdb558f53ccc8bc75def8
Alex Miller <alex.k.miller@gmail.com>
2017-10-12 13:06:34 -0400

    Povcal

---
+++ DevInit/R/P20/povcal_scrape_oct_17.R
@@ -0,0 +1,45 @@
+library(Hmisc)
+library(data.table)
+library(readr)
+
+wd <- "/Users/Alex/Documents/Data/PovCal_Increment"
+setwd(wd)
+
+agg.header <- c(
+  "country"
+  ,"year"
+  ,"type"
+  ,"pl"
+  ,"mean"
+  ,"hc"
+  ,"pg"
+  ,"pg2"
+  ,"watts"
+  ,"gini"
+  ,"median"
+  ,"mld"
+  ,"pop"
+  ,"svyYear"
+  ,"detail"
+)
+
+agg <- read_csv("regional.csv",col_names=agg.header)
+
+agg <- subset(agg,!grepl("--",country,fixed=TRUE))
+agg <- data.table(agg)
+agg.dup <- agg[,.(count=sum(!is.na(type))),by=.(country,year,pl)]
+agg.dup <- subset(agg.dup,count>1)
+agg.dup$id <- paste(agg.dup$country,agg.dup$year,agg.dup$pl)
+agg$id <- paste(agg$country,agg$year,agg$pl)
+all.dups <- agg[which(agg$id %in% agg.dup$id),]
+good.dups <- subset(all.dups,svyYear=="Interpolated")
+agg <- agg[!(agg$id %in% agg.dup$id),]
+agg <- rbind(agg,good.dups)
+agg[,("id"):=NULL]
+
+
+agg <- agg[order(-agg$year,agg$pl,agg$country),]
+
+pcn <- agg
+save(pcn,file="pcn.RData")
+# load("pcn.RData")
