5353c9506a016e20e20b08657e1037e6b3d7f20e
Alex Miller <alex.k.miller@gmail.com>
2018-02-23 12:15:43 -0500

    p20 auto

---
+++ DevInit/povcal_auto/povcal_calc.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pdb
+
+agg = pd.read_csv("C:/Users/Alex/Documents/Data/pcn_auto2/agg.csv")
+smy = pd.read_csv("C:/Users/Alex/Documents/Data/pcn_auto2/smy.csv")
+
+world = agg[agg["RegionTitle"]=="World Total"].copy()
+
+world["diff"] = abs(world["H"]-20)
+
+p20_thresh = round(world[world["diff"]==min(world["diff"])]["PovertyLine"].values[0],2)
+
+p20_data = smy[(smy["PovertyLine"]==p20_thresh) & (smy["displayMode"].isin([0,2,4])) & (smy["CoverageType"].isin([3,5]))]
+
+p20_data.to_csv("C:/Users/Alex/Documents/Data/pcn_auto2/p20.csv",index=False)
